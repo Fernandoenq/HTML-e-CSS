10bb12dbf434c3ec3ce6c02c13f7dc2148430171
Fernando <fernandoasfilho74@gmail.com>
2022-02-17 10:29:13 -0300

    HTML e CSS da microcamp, aula para os alunos

---
+++ antig 06 CSS3/teste2/fedi2/platforms/browser/www/sitescript.js
@@ -0,0 +1,6 @@
+function mostra(){
+            
+    let nomee = document.getElementById("nome").value
+    let emaill = document.getElementById("email").value
+    alert(`Mensagem enviada com sucesso, obrigado ${nomee} fique de olho no email: ${emaill} conversaremos por lá.`)
+}
